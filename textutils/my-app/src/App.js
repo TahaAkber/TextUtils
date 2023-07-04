a772c7754ed86c6307095e6490bce9e93839500c
Taha <taha.akber007@gmail.com>
2023-07-04 19:42:45 +0500

    initial commit

---
+++ textutils/my-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Navigationbar from "./components/Navbar";
+function App() {
+  return (
+    <div className="App">
+      <Navigationbar title="Text-Utils" about="About Us" />
+    </div>
+  );
+}
+
+export default App;
