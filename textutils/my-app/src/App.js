8dbd77165bbc6c87795268cb64fb76533cf17b53
Taha <taha.akber007@gmail.com>
2023-07-16 22:58:35 +0500

    Update App.js

--- textutils/my-app/src/App.js
+++ textutils/my-app/src/App.js
@@ -18,7 +18,6 @@ function App() {
           <Route path="/about" element={<About />}></Route>
         </Routes>
       </BrowserRouter>
-        <Textform heading="Enter Your Text Below" />
     </>
   );
 }
