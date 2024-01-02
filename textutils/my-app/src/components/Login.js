a2f8653da2dea9268a107c892f5925cc4c5c1680
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-01-03 01:45:41 +0500

    Start new app with react router dom

--- textutils/my-app/src/components/Login.js
+++ textutils/my-app/src/components/Login.js
@@ -3,6 +3,7 @@ import { auth, provider } from "./firebase";
 import { signInWithRedirect } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Navigationbar from "./Navbar";
 
 function Login() {
   const navigate = useNavigate();
@@ -14,8 +15,14 @@ function Login() {
   };
   return (
     <div>
-      <h3>SignIn to continue</h3>
-      <button onClick={signin}>Click to Login</button>
+      {!user ? (
+        <div>
+          <h3>SignIn to continue</h3>
+          <button onClick={signin}>Click to Login</button>
+        </div>
+      ) : (
+        <Navigationbar />
+      )}
     </div>
   );
 }
